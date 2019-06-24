fc0675827595d27580e60aaf2010e75fd6034780
ghsmith <geoffrey.hughes.smith@gmail.com>
2019-06-23 22:29:44 -0400

    Added Session.putReagentLot method to change reagent lot number. Not release candidate.

--- hladpb1-webServices/src/main/java/edu/emory/pathology/hladpb1/webservices/Session.java
+++ hladpb1-webServices/src/main/java/edu/emory/pathology/hladpb1/webservices/Session.java
@@ -1,5 +1,7 @@
 package edu.emory.pathology.hladpb1.webservices;
 
+import edu.emory.pathology.hladpb1.imgtdb.AlleleFinder;
+import edu.emory.pathology.hladpb1.imgtdb.HypervariableRegionFinder;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -29,4 +31,19 @@ public class Session {
         request.getSession().invalidate();
     }
 
+    @PUT
+    @Path("reagentLot")
+    @Produces("application/json")
+    public void putReagentLot(@Context HttpServletRequest request, String reagentLotNumber) {
+        AlleleFinder alleleFinder = new AlleleFinder(request.getServletContext().getInitParameter("imgtXmlFileName"));
+        HypervariableRegionFinder hypervariableRegionFinder = new HypervariableRegionFinder(request.getServletContext().getInitParameter("emoryXmlFileName"), reagentLotNumber);
+        alleleFinder.assignHypervariableRegionVariantIds(hypervariableRegionFinder);
+        alleleFinder.assignHypervariableRegionVariantMatches(alleleFinder.getAlleleList().get(0).getAlleleName());
+        alleleFinder.computeCompatInterpretation(hypervariableRegionFinder);
+        ((HttpServletRequest)request).getSession().setAttribute("alleleFinder", alleleFinder);
+        ((HttpServletRequest)request).getSession().setAttribute("hypervariableRegionFinder", hypervariableRegionFinder);
+        SessionFilter.alleleFinder.set(alleleFinder);
+        SessionFilter.hypervariableRegionFinder.set(hypervariableRegionFinder);
+    }
+    
 }
