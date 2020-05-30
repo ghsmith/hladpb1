20e70ee5da3b6079e622c9eebf8d4b9f3e3c8721
ghsmith <geoffrey.hughes.smith@gmail.com>
2020-05-30 01:45:12 -0400

    Added MFI attribute to Allele and HAML parsing web service endpoint.

--- hladpb1-webServices/src/main/java/edu/emory/pathology/hladpb1/webservices/JerseyRestApplication.java
+++ hladpb1-webServices/src/main/java/edu/emory/pathology/hladpb1/webservices/JerseyRestApplication.java
@@ -3,6 +3,7 @@ package edu.emory.pathology.hladpb1.webservices;
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 @ApplicationPath("resources")
 public class JerseyRestApplication extends ResourceConfig {
@@ -10,5 +11,6 @@ public class JerseyRestApplication extends ResourceConfig {
          packages("edu.emory.pathology.hladpb1.webservices");
          register(JacksonFeature.class);
          register(JerseyMapperProvider.class);
+         register(MultiPartFeature.class);
     }
 }
